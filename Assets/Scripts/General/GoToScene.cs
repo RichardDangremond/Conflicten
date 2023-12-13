7c02077e184f931a4bc56f864b2824a026c1d46d
Marl0es <523338@student.saxion.nl>
2023-12-13 11:01:57 +0100

    fade to black 4

--- Assets/Scripts/General/GoToScene.cs
+++ Assets/Scripts/General/GoToScene.cs
@@ -1,19 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GoToScene : MonoBehaviour
+    
 {
+
+    [SerializeField] private Animator animator;
+
+
     //Load a scene based on a string name
     [SerializeField] private string _sceneName;
 
     public void LoadLevel()
     {
         LevelManager.Instance.LoadLevel(_sceneName);
-    }
+        animator.SetTrigger("FadeOut");
 
-    public void QuitLevel()
+    }
+     public void QuitLevel()
     {
         Application.Quit();
+     
     }
+
+
+
 }
+
