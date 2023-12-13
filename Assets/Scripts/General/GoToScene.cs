adb7dc4ad6c17f4b1a6a06be034827e685c32b49
Marl0es <523338@student.saxion.nl>
2023-12-13 11:43:56 +0100

    fade to black 5

--- Assets/Scripts/General/GoToScene.cs
+++ Assets/Scripts/General/GoToScene.cs
@@ -2,30 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class GoToScene : MonoBehaviour
-    
 {
-
     [SerializeField] private Animator animator;
-
-
-    //Load a scene based on a string name
     [SerializeField] private string _sceneName;
+
+    private bool isTransitioning = false; // Add a variable to track the transitioning state
 
     public void LoadLevel()
     {
-        LevelManager.Instance.LoadLevel(_sceneName);
-        animator.SetTrigger("FadeOut");
+        if (!isTransitioning)
+        {
+            isTransitioning = true; // Mark the transition as active to prevent it from being started again
 
+            if (animator != null)
+            {
+                animator.SetTrigger("FadeOut");
+            }
+
+            // Call the transition method after the animation is completed (this requires some setup in the Animator)
+            StartCoroutine(LoadLevelAfterAnimation());
+        }
     }
-     public void QuitLevel()
+
+    IEnumerator LoadLevelAfterAnimation()
+    {
+        yield return new WaitForSeconds(/* Duration of the fade-out animation */);
+
+        LevelManager.Instance.LoadLevel(_sceneName);
+
+        // Reset the transitioning state
+        isTransitioning = false;
+    }
+
+    public void QuitLevel()
     {
         Application.Quit();
-     
     }
-
-
-
 }
 
