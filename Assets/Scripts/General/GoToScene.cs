0456d616aeb0d3e96c69ce4d4111017516f03796
Marl0es <523338@student.saxion.nl>
2023-12-13 12:53:59 +0100

    fade to black 6

--- Assets/Scripts/General/GoToScene.cs
+++ Assets/Scripts/General/GoToScene.cs
@@ -2,44 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.EventSystems;
+
 
 public class GoToScene : MonoBehaviour
+    
 {
-    [SerializeField] private Animator animator;
-    [SerializeField] private string _sceneName;
 
-    private bool isTransitioning = false; // Add a variable to track the transitioning state
+    //Load a scene based on a string name
+    [SerializeField] private string _sceneName;
 
     public void LoadLevel()
     {
-        if (!isTransitioning)
-        {
-            isTransitioning = true; // Mark the transition as active to prevent it from being started again
+    LevelManager.Instance.LoadLevel(_sceneName);
+     
 
-            if (animator != null)
-            {
-                animator.SetTrigger("FadeOut");
-            }
-
-            // Call the transition method after the animation is completed (this requires some setup in the Animator)
-            StartCoroutine(LoadLevelAfterAnimation());
-        }
     }
-
-    IEnumerator LoadLevelAfterAnimation()
-    {
-        yield return new WaitForSeconds(/* Duration of the fade-out animation */);
-
-        LevelManager.Instance.LoadLevel(_sceneName);
-
-        // Reset the transitioning state
-        isTransitioning = false;
-    }
-
-    public void QuitLevel()
+     public void QuitLevel()
     {
         Application.Quit();
+     
     }
+
+
+
 }
 
--- Assets/Scripts/level changer/LevelChanger_script.cs
+++ Assets/Scripts/level changer/LevelChanger_script.cs
@@ -2,31 +2,38 @@
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class LevelChanger_script : MonoBehaviour
 {
-
     [SerializeField] private Animator animator;
-
-
-    //level load script with fade in and out animation
     [SerializeField] private int levelToLoad = 0;
 
-    // Update is called once per frame
+    // Add a variable to track whether fading is in progress
+    private bool isFading = false;
+
     void Update()
     {
-      // put  if statement here (condition to activate FadeToLevel)
+        // Example: check for a condition to activate FadeToLevel
+        if (Input.GetKeyDown(KeyCode.Space) && !isFading)
         {
             FadeToLevel();
         }
     }
-    // trigger animation 
-    [SerializeField] private void FadeToLevel()
+
+    // Trigger the animation
+    private void FadeToLevel()
     {
+        // Set the trigger to start the "FadeOut" animation
         animator.SetTrigger("FadeOut");
+
+        // Set the flag to indicate that fading is in progress
+        isFading = true;
     }
 
+    // Connected to an animation event
     public void OnfadeComplete()
     {
+        // Load the level when the fade-out animation is complete
         SceneManager.LoadScene(levelToLoad);
     }
 }
